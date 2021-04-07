68e7ab52a544049228287f5d5f9338406bfa1d3a
João Pereira <joaopereira.eng.tec@gmail.com>
2021-04-07 15:53:07 +0100

    Adds code to see if the connection with internet is available

--- app/src/main/java/com/example/tasks/service/repository/TaskRepository.kt
+++ app/src/main/java/com/example/tasks/service/repository/TaskRepository.kt
@@ -12,7 +12,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class TaskRepository(val context: Context) {
+class TaskRepository(val context: Context) : BaseRepository(context) {
 
     private val mRemote = RetrofitClient.createService(TaskService::class.java)
 
@@ -32,6 +32,12 @@ class TaskRepository(val context: Context) {
     }
 
     private fun list(call: Call<List<TaskModel>>, listener: APIListener<List<TaskModel>>) {
+
+        if (!isConnectionAvailable(context)) {
+            listener.onFailure(context.getString(R.string.ERROR_INTERNET_CONNECTION))
+            return
+        }
+
         call.enqueue(object : Callback<List<TaskModel>> {
             override fun onResponse(
                 call: Call<List<TaskModel>>,
@@ -60,6 +66,11 @@ class TaskRepository(val context: Context) {
             mRemote.undo(id)
         }
 
+        if (!isConnectionAvailable(context)) {
+            listener.onFailure(context.getString(R.string.ERROR_INTERNET_CONNECTION))
+            return
+        }
+
         call.enqueue(object : Callback<Boolean> {
             override fun onFailure(call: Call<Boolean>, t: Throwable) {
                 listener.onFailure(context.getString(R.string.ERROR_UNEXPECTED))
@@ -78,6 +89,12 @@ class TaskRepository(val context: Context) {
     }
 
     fun create(task: TaskModel, listener: APIListener<Boolean>) {
+
+        if (!isConnectionAvailable(context)) {
+            listener.onFailure(context.getString(R.string.ERROR_INTERNET_CONNECTION))
+            return
+        }
+
         val call: Call<Boolean> = mRemote.create(
             task.priorityId,
             task.description,
@@ -103,6 +120,12 @@ class TaskRepository(val context: Context) {
     }
 
     fun update(task: TaskModel, listener: APIListener<Boolean>) {
+
+        if (!isConnectionAvailable(context)) {
+            listener.onFailure(context.getString(R.string.ERROR_INTERNET_CONNECTION))
+            return
+        }
+
         val call: Call<Boolean> = mRemote.update(
             task.id,
             task.priorityId,
@@ -129,6 +152,12 @@ class TaskRepository(val context: Context) {
     }
 
     fun load(id: Int, listener: APIListener<TaskModel>) {
+
+        if (!isConnectionAvailable(context)) {
+            listener.onFailure(context.getString(R.string.ERROR_INTERNET_CONNECTION))
+            return
+        }
+
         val call: Call<TaskModel> = mRemote.load(id)
 
         call.enqueue(object : Callback<TaskModel> {
@@ -149,6 +178,12 @@ class TaskRepository(val context: Context) {
     }
 
     fun delete(id: Int, listener: APIListener<Boolean>) {
+
+        if (!isConnectionAvailable(context)) {
+            listener.onFailure(context.getString(R.string.ERROR_INTERNET_CONNECTION))
+            return
+        }
+
         val call: Call<Boolean> = mRemote.delete(id)
 
         call.enqueue(object : Callback<Boolean> {
